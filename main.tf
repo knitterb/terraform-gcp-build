c6b909c73c73a287cbf6da0e5d3cb099774f95f8
Brandon Knitter <knitterb@blandsite.org>
2018-10-12 16:00:55 -0700

    initial run through

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  project = "${var.project_id}"
+}
+
+resource "google_cloudbuild_trigger" "build_trigger" {
+  trigger_template {
+    branch_name = "master"
+    project     = "${var.project_id}"
+    repo_name   = "repo"
+  }
+  build {
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = "build -t gcr.io/$PROJECT_ID/$REPO_NAME:$SHORT_SHA -f Dockerfile ."
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = "run gcr.io/$PROJECT_ID/$REPO_NAME:$SHORT_SHA plan -var project_id=$PROJECT_ID"
+    }
+    step {
+      name = "gcr.io/cloud-builders/docker"
+      args = "run gcr.io/$PROJECT_ID/$REPO_NAME:$SHORT_SHA apply -auto-approve -var project_id=$PROJECT_ID"
+    }
+  }
+}
